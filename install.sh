c3c5f6a9fa366284cbce0089ec5f5f9dbe163e66
novalis111 <post@ingo-weseloh.de>
2019-02-01 11:16:55 +0100

    first shot

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+function install() {
+    echo "Syncing dotfiles to home directory..."
+	cp .tmux.conf ~/
+    FC="~/.config/fish/config.fish"
+    if [ -f ${FC} ]; then
+	    grep -q 'direnv hook' ${FC} || echo "eval (direnv hook fish)" >> ${FC}
+    else:
+        echo "eval (direnv hook fish)" >> ${FC}
+    fi
+    echo "done"
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    install;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        install;
+    fi;
+fi;
+unset install;
