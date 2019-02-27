6ff6e373183cf10bfa3425cf66934fd8f7fbc197
Ingo <github@exanto.de>
2019-02-27 12:08:40 +0100

    Add bash extension to bashrc

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 function install() {
     echo "Syncing dotfiles to home directory..."
-	cp .tmux.conf ~/
+    cp .tmux.conf ~/
     FC="~/.config/fish/config.fish"
     if [ -f ${FC} ]; then
 	    grep -q 'direnv hook' ${FC} || echo "eval (direnv hook fish)" >> ${FC}
     else:
         echo "eval (direnv hook fish)" >> ${FC}
     fi
+    grep -q '.dotfiles\/.my_bash' || echo "if [ -f ~/.dotfiles/.my_bash ]; then . ~/.dotfiles/.my_bash; fi" >> ~/.bashrc
     echo "done"
 }
 
