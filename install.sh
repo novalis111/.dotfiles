9aae713e7ccb5e986c448b2acf407da1fb943fc1
novalis111 <post@ingo-weseloh.de>
2019-03-02 11:22:02 +0100

    Add dotenvrc + editor

--- install.sh
+++ install.sh
@@ -4,12 +4,24 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 function install() {
     echo "Syncing dotfiles to home directory..."
-	cp .tmux.conf ~/
-    FC="~/.config/fish/config.fish"
+	cp .tmux.conf ${HOME}/
+    cp .dotenvrc ${HOME}/.config/direnv/direnvrc
+    FC="${HOME}/.config/fish/config.fish"
+    if [ -f /usr/bin/vim ]; then
+        EDITOR="/usr/bin/vim"
+    fi
     if [ -f ${FC} ]; then
 	    grep -q 'direnv hook' ${FC} || echo "eval (direnv hook fish)" >> ${FC}
-    else:
+	    grep -q 'alias ll' ${FC} || echo "alias ll 'ls -lah'" >> ${FC}
+        if [ -n ${EDITOR} ]; then
+            grep -q 'EDITOR' ${FC} || echo "set -x EDITOR $EDITOR" >> ${FC}
+        fi
+    else
         echo "eval (direnv hook fish)" >> ${FC}
+        echo "alias ll 'ls -lah'" >> ${FC}
+        if [ -n ${EDITOR} ]; then
+            echo "set -x EDITOR $EDITOR" >> ${FC}
+        fi
     fi
     echo "done"
 }
