3660652da3598994611907791f655ba4d3a1e914
novalis111 <post@ingo-weseloh.de>
2019-03-04 13:54:04 +0100

    Tinkering

--- install.sh
+++ install.sh
@@ -13,18 +13,13 @@ function install() {
     fi
     if [ -f ${FC} ]; then
 	    grep -q 'direnv hook' ${FC} || echo "eval (direnv hook fish)" >> ${FC}
-	    grep -q 'alias ll' ${FC} || echo "alias ll 'ls -lah'" >> ${FC}
         if [ -n ${EDITOR} ]; then
             grep -q 'EDITOR' ${FC} || echo "set -x EDITOR $EDITOR" >> ${FC}
         fi
       # Add aliases to fish
       grep -q 'my_aliases' ${FC} || cat .my_aliases >> ${FC}
     else
-        echo "eval (direnv hook fish)" >> ${FC}
-        echo "alias ll 'ls -lah'" >> ${FC}
-        if [ -n ${EDITOR} ]; then
-            echo "set -x EDITOR $EDITOR" >> ${FC}
-        fi
+	echo "No fish shell config found, please install + create ${FC} to use it"
     fi
     grep -q '.dotfiles\/.my_bash' ${HOME}/.bashrc || echo "if [ -f ~/.dotfiles/.my_bash ]; then . ~/.dotfiles/.my_bash; fi" >> ${HOME}/.bashrc
     echo "done"
