950e3df1b697a6b7b863a6a5b109edc9471dca45
novalis111 <post@ingo-weseloh.de>
2019-03-04 14:34:45 +0100

    Tinkering

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 function install() {
-    echo "Syncing dotfiles to home directory..."
-	cp .tmux.conf ${HOME}/
-    cp .dotenvrc ${HOME}/.config/direnv/direnvrc
+    DC="${HOME}/.config/direnv/direnvrc"
     FC="${HOME}/.config/fish/config.fish"
     BC="${HOME}/.dotfiles/.my_bash"
+    echo "Syncing dotfiles to home directory..."
+    cp .tmux.conf ${HOME}/
+    if [ -d ${HOME}/.config/direnv ]; then
+        cp .dotenvrc ${HOME}/.config/direnv/direnvrc
+    else
+        echo "Notice: direnv not installed, skipping config"
+    fi
     if [ -f /usr/bin/vim ]; then
         EDITOR="/usr/bin/vim"
     fi
     if [ -f ${FC} ]; then
 	    grep -q 'direnv hook' ${FC} || echo "eval (direnv hook fish)" >> ${FC}
-	    grep -q 'alias ll' ${FC} || echo "alias ll 'ls -lah'" >> ${FC}
         if [ -n ${EDITOR} ]; then
             grep -q 'EDITOR' ${FC} || echo "set -x EDITOR $EDITOR" >> ${FC}
         fi
       # Add aliases to fish
       grep -q 'my_aliases' ${FC} || cat .my_aliases >> ${FC}
     else
-        echo "eval (direnv hook fish)" >> ${FC}
-        echo "alias ll 'ls -lah'" >> ${FC}
-        if [ -n ${EDITOR} ]; then
-            echo "set -x EDITOR $EDITOR" >> ${FC}
-        fi
+	     echo "Notice: fish shell not installed, skipping config"
     fi
     grep -q '.dotfiles\/.my_bash' ${HOME}/.bashrc || echo "if [ -f ~/.dotfiles/.my_bash ]; then . ~/.dotfiles/.my_bash; fi" >> ${HOME}/.bashrc
     wget https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim -O ~/.vimrc
-    echo "done"
+    echo "Done :)"
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
